e187afc2fed0c20741859efe323ccc4a9b1045b1
Pradyumna Paralikar <pradyumna.paralikar@citrix.com>
2022-12-16 16:23:40 +0530

    binary serach

---
+++ searching_sorting/binary_search.py
@@ -0,0 +1,26 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+low = 1
+high = len(a)
+print (high)
+
+def binary_search(key, low, high, a):
+    
+    
+    
+    if (low > high):
+        print("element not found")
+        return
+    
+    mid = low + int((high - low) / 2)
+    
+    if (a[mid] == key):
+        print ("Element found")
+    elif (a[mid] > key):
+        high = mid -1
+        binary_search(key, low, high, a)
+    elif (a[mid] < key):
+        low = mid + 1
+        binary_search(key, mid, high, a)
+        
+binary_search(-8, 0, 9, a) 
